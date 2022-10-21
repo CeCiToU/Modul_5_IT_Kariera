f1113b806e86698e920d2a29b101b821c15bc46c
Tsvetoslav <cveti3060@gmail.com>
2022-10-21 12:09:08 +0300

    06.1.4

---
+++ IT Kariera Modul 5/06-Работа-с-обекти/06.1. Итератори-упражнения/Problem_2/Solution_2/ListyIterator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator2
+{
+    class ListyIterator<T> : IEnumerable<T>
+    {
+        List<T> elements;
+        int index;
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+            index = 0;
+        }
+
+        public ListyIterator() : this(new List<T>()){}
+
+        public bool Move()
+        {
+            if (index + 1 < elements.Count)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < elements.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{elements[index]}");
+                Console.ResetColor();
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Invalid Operation!");
+                Console.ResetColor();
+            }
+        }
+
+        public void PrintAll()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"{elements[i]} ");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ IT Kariera Modul 5/06-Работа-с-обекти/06.1. Итератори-упражнения/Problem_2/Solution_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] command = Console.ReadLine().Split().ToArray();
+            ListyIterator<string> listy = new ListyIterator<string>();
+            while (command[0] != "END")
+            {
+                switch (command[0])
+                {
+                    case "Create":
+                        List<string> inp = new List<string>();
+                        for(int i = 1; i < command.Length; i++)
+                        {
+                            inp.Add(command[i]);
+                        }
+                        if (inp.Count != 0)
+                        {
+                            listy = new ListyIterator<string>(inp);
+                        }
+                        break;
+                    case "Move":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(listy.Move());
+                        Console.ResetColor();
+                        break;
+                    case "Print":
+                        listy.Print();
+                        break;
+                    case "HasNext":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(listy.HasNext());
+                        Console.ResetColor();
+                        break;
+                    case "PrintAll":
+                        listy.PrintAll();
+                        break;
+                }
+                command = Console.ReadLine().Split().ToArray();
+            }
+        }
+    }
+}
