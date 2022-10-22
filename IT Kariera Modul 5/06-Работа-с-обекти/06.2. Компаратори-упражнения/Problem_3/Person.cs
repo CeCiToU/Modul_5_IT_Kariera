2528d47ca9d894389a371a7d4639f5d99bfe2bcd
Tsvetoslav <cveti3060@gmail.com>
2022-10-22 12:59:37 +0300

    Comparer and Comparable

---
+++ IT Kariera Modul 5/06-Работа-с-обекти/06.2. Компаратори-упражнения/Problem_3/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparer_Exercises
+{
+    class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string cityName;
+
+        public Person(string name, int age, string cityName)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.CityName = cityName;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string CityName { get => cityName; set => cityName = value; }
+
+        public int CompareTo(Person other)
+        {
+            int res = this.Name.CompareTo(other.Name);
+            if(res == 0)
+            {
+                res = this.Age.CompareTo(other.Age);
+            }
+            if(res == 0)
+            {
+                res = this.CityName.CompareTo(other.CityName);
+            }
+            return res;
+        }
+    }
+}
---
+++ IT Kariera Modul 5/06-Работа-с-обекти/06.2. Компаратори-упражнения/Problem_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Comparer_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            string[] command;
+            while((command = Console.ReadLine().Split().ToArray())[0] != "END")
+            {
+                Person currentPerson = new Person(command[0],int.Parse(command[1]), command[2]);
+                persons.Add(currentPerson);
+            }
+            int n = int.Parse(Console.ReadLine());
+            int equal = 0;
+            int nonEqual = 0;
+            for (int i = 0; i < persons.Count; i++)
+            {
+                if (persons[n - 1].CompareTo(persons[i]) == 0)
+                {
+                    equal++;
+                }
+                else
+                {
+                    nonEqual++;
+                }
+            }
+            if (equal == 1)
+            {
+                Console.WriteLine("No matches!");
+            }
+            else
+            {
+                Console.WriteLine($"{equal} {nonEqual} {persons.Count}");
+            }
+        }
+    }
+}
