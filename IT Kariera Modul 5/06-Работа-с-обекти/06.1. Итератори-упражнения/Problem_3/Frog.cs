3f9a84e421251a3040bb7715fa608aae167b7b80
Tsvetoslav <cveti3060@gmail.com>
2022-10-22 12:27:37 +0300

    Task_Frog

---
+++ IT Kariera Modul 5/06-Работа-с-обекти/06.1. Итератори-упражнения/Problem_3/Frog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FROG
+{
+    class Frog : IEnumerable<int>
+    {
+        List<int> frogs = new List<int>();
+        public Frog(List<int> input)
+        {
+            frogs = input;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int i = 0; i < frogs.Count; i++)
+            {
+                if(i % 2 == 0)
+                yield return frogs[i];
+            }
+            for(int i = frogs.Count - 1; i > 0; i--)
+            {
+                if(i % 2 == 1)
+                yield return frogs[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ IT Kariera Modul 5/06-Работа-с-обекти/06.1. Итератори-упражнения/Problem_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FROG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> frogs = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Frog frog = new Frog(frogs);
+            var cm = frog.GetEnumerator();
+            foreach(var f in frog)
+            {
+                Console.Write($"{f} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
